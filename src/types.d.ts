4f195429daa78e03feef2d6a4045bb17ef8d64e8
fcamblor <fcamblor@gmail.com>
2020-05-21 18:50:42 +0200

    added some ts file extensions declations for custom imports

--- src/types.d.ts
+++ src/types.d.ts
@@ -5,3 +5,23 @@ declare global {
         constructibleStyleSheets: {[name:string]: CSSResult};
     }
 }
+
+/* Custom file extensions declarations for importing */
+
+declare module '*.css';
+declare module '*.svg' {
+    const ref: string;
+    export default ref;
+}
+declare module '*.png' {
+    const ref: string;
+    export default ref;
+}
+declare module '*.jpg' {
+    const ref: string;
+    export default ref;
+}
+declare module '*.jpeg' {
+    const ref: string;
+    export default ref;
+}
